f879567c14f3bdce205c5f1b495dd37bc4c002d8
brojoon <ydngjink1234@gmail.com>
2023-09-13 21:08:19 +0900

    feat: gql-client 셋업, 테스트

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,4 +1,15 @@
+import {  gql, useQuery } from '@apollo/client';
+
+const GET_READ = gql`
+    query GetLead {
+        id
+    }
+`;
+
 export default function Read(props) {
+    const [readId, setReadId] = useState('')
+    const {data, loading, error} = useQuery(GET_READ);
+
     return (
         <>
          <h2>Read</h2>
