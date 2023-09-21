7e09f6c355fc5e8718766a44e9bf3c9ea00b3f32
brojoon <ydngjink1234@gmail.com>
2023-09-22 02:53:39 +0900

    react-router v6: Navigate

--- cra/src/pages/About.js
+++ cra/src/pages/About.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { useNavigate, Navigate } from "react-router-dom";
 
 const About = () => {
+  const navigate = useNavigate();
+  const handleClick = () => navigate("/", { replace: true });
+
   return (
     <>
       <h1>소개 페이지</h1>
+      <button onClick={handleClick}>Go back Main</button>
+      <Navigate to="/home" replace />
     </>
   );
 };
--- cra/src/pages/Main.js
+++ cra/src/pages/Main.js
@@ -3,7 +3,7 @@ import React from "react";
 const Main = () => {
   return (
     <>
-      <h1>Main</h1>
+      <h1>메인 페이지</h1>
     </>
   );
 };
