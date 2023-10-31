9f6610eed8a3f763920f9e42ce0b16a8e509d647
brojoon <ydngjink1234@gmail.com>
2023-10-31 09:40:45 +0900

    feat: zustand 설치, counter 예제

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,8 +1,18 @@
+"use client"
 import Image from 'next/image'
 import styles from './page.module.css'
+import create from 'zustand'
+import useNumberBaseStore  from './hooks/store'
 
 export default function Home() {
+  const { numberA, numberB, increaseNumberA, increaseNumberB } = useNumberBaseStore();
+  
   return (
-   <>Hello, Nextjs!</>
+   <>
+    <h2>numberA : {numberA}</h2>
+    <h2>numberB : {numberB}</h2>
+    <button onClick={increaseNumberA}>A 증가</button>
+    <button onClick={() => increaseNumberB(3)}>B 증가</button>
+   </>
   )
 }
